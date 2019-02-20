d83db0c8895582df6fca8f1b647f4d0d95d258ce
Andrisoel <andrisoelto@gmail.com>
2019-02-20 08:51:15 +0700

    small update

--- app/src/main/java/com/gogoy/data/collections/ArrayListItem_Dummy.kt
+++ app/src/main/java/com/gogoy/data/collections/ArrayListItem_Dummy.kt
@@ -9,6 +9,18 @@ class ArrayListItem_Dummy {
             ItemModel("1", "Gedang", "1000", "Sang Gedang", R.drawable.gedang),
             ItemModel("2", "Bawang", "3000", "PT Bawang", R.drawable.bawang),
             ItemModel("3", "Paprika", "5000", "PT Lombok", R.drawable.paprika),
+            ItemModel("4", "Lombok Abang", "7000", "PT Lombok", R.drawable.lombok_abang),
+            ItemModel("1", "Gedang", "1000", "Sang Gedang", R.drawable.gedang),
+            ItemModel("2", "Bawang", "3000", "PT Bawang", R.drawable.bawang),
+            ItemModel("3", "Paprika", "5000", "PT Lombok", R.drawable.paprika),
+            ItemModel("4", "Lombok Abang", "7000", "PT Lombok", R.drawable.lombok_abang),
+            ItemModel("1", "Gedang", "1000", "Sang Gedang", R.drawable.gedang),
+            ItemModel("2", "Bawang", "3000", "PT Bawang", R.drawable.bawang),
+            ItemModel("3", "Paprika", "5000", "PT Lombok", R.drawable.paprika),
+            ItemModel("4", "Lombok Abang", "7000", "PT Lombok", R.drawable.lombok_abang),
+            ItemModel("1", "Gedang", "1000", "Sang Gedang", R.drawable.gedang),
+            ItemModel("2", "Bawang", "3000", "PT Bawang", R.drawable.bawang),
+            ItemModel("3", "Paprika", "5000", "PT Lombok", R.drawable.paprika),
             ItemModel("4", "Lombok Abang", "7000", "PT Lombok", R.drawable.lombok_abang)
         )
     }
