2e08dc28ae9567c2392bf38f167cb3882aa8dda6
Andrisoel <andrisoelto@gmail.com>
2019-02-26 12:46:17 +0700

    small change

--- app/src/main/java/com/gogoy/utils/Currency.kt
+++ app/src/main/java/com/gogoy/utils/Currency.kt
@@ -3,7 +3,7 @@ package com.gogoy.utils
 import java.text.NumberFormat
 import java.util.*
 
-fun toRupiah(string: Double): String {
+fun toRupiah(string: Int): String {
     val localeID = Locale("in", "ID")
     val formatRupiah = NumberFormat.getCurrencyInstance(localeID)
 
